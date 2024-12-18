2381d37067de75a56b3e803d4e520c7ae9663e5c
Shushant-Priyadarshi <profshushant@gmail.com>
2024-12-18 23:15:48 +0530

    workflow done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import User1 from "./passingWithContextAPI/User1";
-// import Child from "./propdrilling/Child";
-
 function App() {
-  return (
-    <>
-      <User1/>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
